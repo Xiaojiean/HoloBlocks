d4d18a8c642900999fb9906ee4ab733bb593c5d9
Yichen Yao <hansenyao.yy@gmail.com>
2017-02-16 19:33:55 -0500

    Intial commit; replacement of old repository; implementation of voice commands.

---
+++ Assets/Scripts/Model.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject sphere;
+
+    private void CreateGameObject(GameObject obj)
+    {
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        Instantiate(obj, headPosition + gazeDirection, obj.transform.rotation);
+    }
+
+    private void deleteAll(string tag)
+    {
+        GameObject[] elements = GameObject.FindGameObjectsWithTag(tag);
+        foreach (GameObject element in elements)
+        {
+            Destroy(element);
+        }
+    }
+
+    public void OnCreateCube()
+    {
+        CreateGameObject(cube);
+    }
+
+    public void OnCreateSphere()
+    {
+        CreateGameObject(sphere);
+    }
+
+    public void OnDeleteAllCubes()
+    {
+        deleteAll("Cube");
+    }
+
+    public void OnDeleteAllSpheres()
+    {
+        deleteAll("Sphere");
+    }
+
+    public void OnClearCanvas()
+    {
+        deleteAll("Cube");
+        deleteAll("Sphere");
+    }
+
+}
---
+++ Assets/Scripts/VoiceController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceController : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        keywords.Add("Create cube", () =>
+        {
+            this.BroadcastMessage("OnCreateCube");
+        });
+
+        keywords.Add("Create sphere", () =>
+        {
+            this.BroadcastMessage("OnCreateSphere");
+        });
+
+        keywords.Add("Delete all cubes", () =>
+        {
+            this.BroadcastMessage("OnDeleteAllCubes");
+        });
+
+        keywords.Add("Delete all spheres", () =>
+        {
+            this.BroadcastMessage("OnDeleteAllSpheres");
+        });
+
+        keywords.Add("Clear canvas", () =>
+        {
+            this.BroadcastMessage("OnClearCanvas");
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+	
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
