4dc86e79e4b88686df68ce0dd9f73c81f93f52e3
Yichen Yao <hansenyao.yy@gmail.com>
2017-02-18 00:27:51 -0500

    Working start button on menu screen.

---
+++ Assets/Scripts/StartMenu/StartButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    // Called by GazeGestureController when the user performs a Select gesture
+    void OnSelect()
+    {
+        SceneManager.LoadScene("Canvas");
+    }
+}
