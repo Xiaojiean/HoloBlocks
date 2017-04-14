5cec2876ae711ee0aebf1f9d1e94a47d13a9201b
Yichen Yao <hansenyao.yy@gmail.com>
2017-04-13 23:09:40 -0400

    Add collision sound effects.

---
+++ Assets/Scripts/Canvas/PrefabSounds.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSounds : MonoBehaviour {
+    AudioSource audioSource = null;
+    public AudioClip impactClip = null;
+
+    bool rolling = false;
+
+    void Start()
+    {
+        // Add an AudioSource component and set up some defaults
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.spatialize = true;
+        audioSource.spatialBlend = 1.0f;
+        audioSource.dopplerLevel = 0.0f;
+        audioSource.rolloffMode = AudioRolloffMode.Logarithmic;
+        audioSource.maxDistance = 20f;
+        audioSource.clip = impactClip;
+    }
+
+    // Occurs when this object starts colliding with another object
+    void OnCollisionEnter(Collision collision)
+    {
+        // Play an impact sound if the sphere impacts strongly enough.
+        if (collision.relativeVelocity.magnitude >= 0.1f)
+        {
+            audioSource.Play();
+        }
+    }
+}
